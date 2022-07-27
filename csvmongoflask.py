962d5ce0edba99a43ebe274599928c9034034c92
Tiffany Jones <thisismstiffanyjones@gmail.com>
2022-07-26 19:44:26 -0500

    Adding a json file of the data

--- csvmongoflask.py
+++ csvmongoflask.py
@@ -2,23 +2,33 @@ from flask import Flask, render_template
 from numpy import append
 import pymongo
 import pandas as pd
+import csv
 
 app = Flask(__name__)
 
 client = pymongo.MongoClient("mongodb://localhost:27017")
 
-df = pd.read_csv("data.csv")
+df = pd.read_csv("data/data.csv")
 
 db = client.data_db
-db.collection.insert_many(df.to_dict("records"))
+cat = db.collection.insert_many(df.to_dict("records"))
 
+
+dict_from_csv={}
+    with open ('data/data.csv',mode='r') as inp:
+        reader = csv.reader(inp)
+        dict_from_csv = {rows[0]:rows[1] for rows in reader}
+        print(dict_from_csv)
+
+        
 @app.route("/")
 def home():
-    newDict=[]
-    for row in db.collection.find()[:10]:
-        newDict.append(row["Name"])
-    returndict = {"results":newDict}
-    return returndict
+    
+    # newDict=[]
+    # for row in db.collection.find()[:10]:
+    #     newDict.append(row["Name"])
+    #     returndict = {"results":newDict}
+    # return returndict
 
 if __name__ == "__main__":
     app.run(debug=True)
